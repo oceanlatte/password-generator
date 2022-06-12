5ac5e50e992f38ce9c98d492524abd002408e3ae
ocean <osciris.veliz@gmail.com>
2022-06-12 12:27:30 -0500

    add separate functions to fix local scope

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,11 @@ var lowerCase = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 
-var lowerOutput = "";
+var passwordOutput = "";
 
-for (let i = 0; i < numbers.length; i++) {
+for (let i = 0; i < passwordLength; i++) {
   var getNumber = numbers[i];
+  //run the array if generateNumbers = true
 }
 
 for (let i = 0; i < lowerCase.length; i++) {
@@ -25,7 +26,6 @@ for (let i = 0; i < specialCharacters.length; i++) {
   var getSpecial = specialCharacters[i];
 }
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -37,40 +37,44 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function passwordLength() {
+  var characters = parseInt(prompt(
+    "How many characters should your password be? Please choose a number between 8 - 128."
+  ));
+  console.log(characters);
+    if (characters < 8 || characters > 128 || characters === null || characters == "") {
+      alert("Password must be between 8 - 128. Please choose a valid length.");
+      passwordLength();
+    } else {
+      return passwordLength;
+    }
+};
+
+function confirmPrompts() {
+  if (window.confirm("Should your password contain numbers? Choose OK for yes.")) {
+     
+    passwordOutput = passwordOutput.concat(numbers);
+    console.log(passwordOutput);
+    }
+  
+  var generateLower = window.confirm(
+    "Should your password contain lowercase letters? Choose OK for yes."
+  );
+  
+  var generateUpper = window.confirm(
+    "Should your password contain uppercase letters? Choose OK for yes."
+  );
+
+  var generateSymbols = window.confirm(
+    "Should your password contain symbols? Choose OK for yes."
+  );
+};
 
 // Criteria for password prompts
-function generatePassword() {
-  var passwordLength = prompt(
-    "How many characters should your password be? Please choose a nubmer between 8 - 128."
-    );
-    
-  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null || passwordLength == "") {
-    alert("Password must be 8 - 128 characters. Please choose a valid length.");
-    generatePassword();
-  }
-  else {
-    var generateNumbers = window.confirm(
-      "Should your password contain numbers? Choose OK for yes."
-    ); 
-      if (generateNumbers) {
-        return getNumber;
-      }
-      
-
-    var generateLower = window.confirm(
-      "Should your password contain lowercase letters? Choose OK for yes."
-    );
-    
-    var generateUpper = window.confirm(
-      "Should your password contain uppercase letters? Choose OK for yes."
-    );
-
-    var generateSymbols = window.confirm(
-      "Should your password contain symbols? Choose OK for yes."
-    );
-  } 
+function generatePassword(length) {
+  passwordLength();
+  confirmPrompts();
   // call the concatenated string functions
-
-
-  // return generatePassword;    
-};
+  
+  // return generatePassword;   
+  } 
