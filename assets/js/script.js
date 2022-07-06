f79f1a9c992ca0b50eedf4bc03c966f7d56b7415
ocean <osciris.veliz@gmail.com>
2022-07-05 23:40:34 -0500

    reset textarea if another password is generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-}
-
 // Character Arrays
 var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCase = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 
-var passwordOutput = "";
 var confirmedArray = [];
 var characters = 8;
+var passwordOutput = "";
 
 function passwordLength() {
   characters = parseInt(prompt(
     "How many characters should your password be? Please choose a number between 8 - 128."
   ));
   console.log(characters);
-    if (characters < 8 || characters > 128 || characters === null || characters == "") {
+    if (characters < 8 || characters > 128 || characters == NaN || characters == "") {
       alert("Password must be between 8 - 128. Please choose a valid length.");
       passwordLength();
     }
 };
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  debugger;
+  if (!passwordText.value) {
+    passwordText.value = password;
+  }
+  // reset textarea if new password is generated
+  else {
+    passwordText.value = "";
+    passwordText.value = password;
+  }
+};
+
 function confirmPrompts() {
-  confirmedArray = [];
   if (window.confirm("Should your password contain numbers? Choose OK for yes.")) {
     //run the array if generateNumbers = true
     confirmedArray = confirmedArray.concat(numbersArray);
-    console.log(confirmedArray, "numbers");
   }
   
   if (window.confirm("Should your password contain lowercase letters? Choose OK for yes.")) {
     confirmedArray = confirmedArray.concat(lowerCase);
-    console.log(confirmedArray, "lowercase");
   }
   
   if (window.confirm("Should your password contain uppercase letters? Choose OK for yes.")) {
     confirmedArray = confirmedArray.concat(upperCase);
-    console.log(confirmedArray, "upper");
   }
 
   if (window.confirm("Should your password contain symbols? Choose OK for yes.")) {
     confirmedArray = confirmedArray.concat(specialCharacters);
-    console.log(confirmedArray, "symbols");
   }
+  // if not options are chosen then use lowercase as default
   else if (confirmedArray.length == 0) {
     confirmedArray = confirmedArray.concat(lowerCase);
     console.log(confirmedArray);
@@ -62,14 +62,22 @@ function confirmPrompts() {
 };
 
 function generatePassword() {
+  // reset values before making another password
+  confirmedArray = [];
+  passwordOutput = "";
+  
+  // call parameter functions
   passwordLength();
   confirmPrompts();
-  // call the concatenated array
+
+  // call the concatenated array to generate randomized password
   for (var i = 0; i < characters; i++) {
       var getRandomIndex = Math.floor(Math.random() * confirmedArray.length);
-      passwordOutput = passwordOutput + confirmedArray[getRandomIndex];
+      passwordOutput += confirmedArray[getRandomIndex];
     }
-
-  // return generated password;
+  // return newly generated password;
   return passwordOutput; 
-}
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
